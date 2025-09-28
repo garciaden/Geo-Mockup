ca93d729365a7ef687bbb21ae46dc8bd9643cf8a
Carlos <carlosgcabadas@gmail.com>
2025-09-28 01:04:02 +0000

    Organized projects into own module!

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,20 +1,12 @@
-from flask import app, render_template
+from flask import app, render_template, redirect, url_for
 from app.main import bp
 
 @bp.route('/')
-@bp.route('/index')
+#@bp.route('/index')
 def index():
-    
-    
+        
     return render_template("main/index.html", title="Home")
 
-
-@bp.route('/projects')
-def projects():
-    projects = [
-        {"title": "Tephra Analysis", "owner": "Matthew Kenner", "status": "Ongoing"},
-        {"title": "Sediment Study", "owner": "Ian Keitlan", "status": "Completed"},
-        {"title": "Volcanic Glass Imaging", "owner": "Killian Bertsch", "status": "In Review"},
-        {"title": "Lake Sample Processing", "owner": "Carlos Cortes Garcia", "status": "Completed"}
-    ]
-    return render_template("main/projects.html", title="Home", projects=projects)
+@bp.route('/login')
+def fake_login():
+    return redirect(url_for('auth.login'))
---
+++ app/projects/routes.py
@@ -0,0 +1,12 @@
+from flask import app, render_template
+from app.projects import bp
+
+@bp.route('/')
+def projects():
+    projects = [
+        {"title": "Tephra Analysis", "owner": "Matthew Kenner", "status": "Ongoing"},
+        {"title": "Sediment Study", "owner": "Ian Keitlan", "status": "Completed"},
+        {"title": "Volcanic Glass Imaging", "owner": "Killian Bertsch", "status": "In Review"},
+        {"title": "Lake Sample Processing", "owner": "Carlos Cortes Garcia", "status": "Completed"}
+    ]
+    return render_template("projects/projects.html", title="Projects", projects=projects)
